870a10cec6892373e2c00e8c6de2457d332fb86b
Sangini Shah <sanshah13@yahoo.com>
2015-02-17 21:05:45 -0500

    added a tiny main

---
+++ pa2/main.c
@@ -0,0 +1,14 @@
+/*
+	Sangini Shah and Rob Williams
+	CS214 Systems Programming
+	Programming Assignment 2- Sorted List
+	
+	main.c
+*/
+
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+
+	return 0;
+} 
